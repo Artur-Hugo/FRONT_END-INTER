28ebad62cdf40f3a66cffebd832962af604569da
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-12-10 16:40:58 -0300

    signup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { Wrapper, Background, InputContainer, ButtonContainer } from "./styles";
 
-const SignUp = () => {
+import Card from "../../components/Card";
+
+import background from '../../assets/images/background-login.jpg'
+
+import logoInter from '../../assets/images/Inter-orange.png'
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { useNavigate,Link } from "react-router-dom";
+const SignIn = () => {
+
+    const navigate = useNavigate()
+    //quando logar vai ir aonde foi redirecionando
+
+    const handleToSignUp = () =>{
+        navigate('/dashbaord');
+    }
+
     return (
-        <div>
-            SignUp
-        </div>
+        <Wrapper>
+            <Background image={background}/>
+            <Card width="403px">
+            <img src={logoInter} width={172} height={61} alt="logo inter"/>
+
+            <InputContainer>
+            <Input placeholder="NOME"/>
+            <Input placeholder="SOBRENOME"/>
+            <Input placeholder="EMAIL"/>
+            <Input placeholder="SENHA" type="password"/>
+            <Input placeholder="CONFIRMAR SENHA" type="password"/>
+
+            <ButtonContainer>
+                <Button type="button" onClick={handleToSignUp}>Entrar</Button>
+                <p>Já tem uma conta? <Link to="/signin">Entre Já</Link></p>
+            </ButtonContainer>
+
+            </InputContainer>
+            </Card>
+        </Wrapper>
     )
 }
 
-export default SignUp
+export default SignIn;
